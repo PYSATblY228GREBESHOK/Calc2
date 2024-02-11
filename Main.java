e7ac5dc402b32288932e7c2c2c5afff70511d4fa
Зеленский <144904112+PYSATblY228GREBESHOK@users.noreply.github.com>
2024-02-11 18:34:36 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -9,20 +9,19 @@ public class Main { // Создание класса Main
     }
 
     // Метод для вычисления расстояния между двумя точками по их географическим координатам
-    public static double calculateDistance(double Cdt1, double Cdn1, double Cdt2, double Cdn2) {
+    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
 
         // Преобразование координат в радианы
-        Cdt1 = toRadians(Cdt1);
-        Cdn1 = toRadians(Cdn1);
-        Cdt2 = toRadians(Cdt2);
-        Cdn2 = toRadians(Cdn2);
+        lat1 = toRadians(lat1);
+        lon1 = toRadians(lon1);
+        lat2 = toRadians(lat2);
+        lon2 = toRadians(lon2);
 
-
-        double dLat = Cdt2 - Cdt1;// Разница между широтами и долготами
-        double dLon = Cdn2 - Cdn1;
+        double dLat = lat2 - lat1; // Разница между широтами и долготами
+        double dLon = lon2 - lon1;
 
         // Формула гаверсинусов для вычисления расстояния на сфере
-        double i = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(Cdt1) * Math.cos(Cdn2) * Math.pow(Math.sin(dLon / 2), 2);
+        double i = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dLon / 2), 2);
         double n = 2 * Math.atan2(Math.sqrt(i), Math.sqrt(1 - i));
 
         return radiusEarth * n; // Вычисление расстояния
@@ -33,18 +32,18 @@ public class Main { // Создание класса Main
 
         // Ввод координат первой точки
         System.out.println("Введите широту первой точки (в градусах):");
-        double Cdt1 = scanner.nextDouble();
+        double lat1 = scanner.nextDouble();
         System.out.println("Введите долготу первой точки (в градусах):");
-        double Cdn1 = scanner.nextDouble();
+        double lon1 = scanner.nextDouble();
 
         // Ввод координат второй точки
         System.out.println("Введите широту второй точки (в градусах):");
-        double Cdt2 = scanner.nextDouble();
+        double lat2 = scanner.nextDouble();
         System.out.println("Введите долготу второй точки (в градусах):");
-        double Cdn2 = scanner.nextDouble();
+        double lon2 = scanner.nextDouble();
 
         // Вычисление расстояния
-        double distance = calculateDistance(1, 1, 2, 2);
+        double distance = calculateDistance(lat1, lon1, lat2, lon2);
         System.out.println("Расстояние между вашими точками: " + distance + " км");
 
         scanner.close();
